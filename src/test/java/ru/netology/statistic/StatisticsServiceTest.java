ffecd084cfcdafbb05b6894cdd96fbfd3b9df402
Andrey K <87851330+kvasoff@users.noreply.github.com>
2023-05-11 19:11:46 +0300

    Update StatisticsServiceTest.java

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -8,7 +8,7 @@ public class StatisticsServiceTest {
 
     @Test
     void findMax() {
-        StatisticsService service = StatisticsService();
+        StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
